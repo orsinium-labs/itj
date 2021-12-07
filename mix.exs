dc8973ffc8ec64ca965f6ae04250ccccff31c144
gram <mail@orsinium.dev>
2021-12-07 15:26:29 +0100

    fix phoenix running

--- mix.exs
+++ mix.exs
@@ -11,15 +11,13 @@ defmodule ITJ.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools, :os_mon],
       mod: {ITJ.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ecto_sqlite3, "~> 0.7.1"},
@@ -27,13 +25,12 @@ defmodule ITJ.MixProject do
       {:floki, "~> 0.32.0"},
       {:httpoison, "~> 1.8"},
       {:jason, "~> 1.2"},
-      {:phoenix, "~> 1.6.2"},
-      {:phoenix_live_dashboard, "~> 0.6.2"},
       {:phoenix_html, "~> 3.1.0"},
+      {:phoenix_live_dashboard, "~> 0.6.2"},
+      {:phoenix, "~> 1.6.2"},
+      {:plug_cowboy, "~> 2.5.2"},
       {:telemetry_metrics, "~> 0.6.1"},
       {:telemetry_poller, "~> 1.0.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
