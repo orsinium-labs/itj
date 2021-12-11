e98383ce238538aefbb0a4d6498f17ffb9d18079
gram <mail@orsinium.dev>
2021-12-11 11:25:51 +0100

    filter results by search form

--- lib/itj_web/components/offer_list.ex
+++ lib/itj_web/components/offer_list.ex
@@ -1,7 +1,33 @@
 defmodule ITJWeb.Components.OfferList do
   use Phoenix.LiveComponent
+  import Ecto.Query
 
   def render(assigns) do
     Phoenix.View.render(ITJWeb.ComponentView, "offer_list.html", assigns)
+  end
+
+  def handle_event("offer_search", %{"search" => search}, socket) do
+    offers =
+      from(o in ITJ.Offer, join: c in assoc(o, :company), preload: [company: c])
+      |> apply_filters(search)
+      |> ITJ.Repo.all()
+
+    {:noreply, assign(socket, :offers, offers)}
+  end
+
+  defp apply_filters(query, filters) do
+    query |> apply_remote(filters)
+  end
+
+  defp apply_remote(query, %{"remote" => "yes"}) do
+    where(query, remote: true)
+  end
+
+  defp apply_remote(query, %{"remote" => "no"}) do
+    where(query, remote: false)
+  end
+
+  defp apply_remote(query, _) do
+    query
   end
 end
