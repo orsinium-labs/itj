192ceba3b0a0751885813bf59e4df7b17d6741c3
gram <mail@orsinium.dev>
2021-12-12 12:27:12 +0100

    send pagination events

--- lib/itj_web/components/pagination.ex
+++ lib/itj_web/components/pagination.ex
@@ -4,4 +4,10 @@ defmodule ITJWeb.Components.Pagination do
   def render(assigns) do
     Phoenix.View.render(ITJWeb.ComponentView, "pagination.html", assigns)
   end
+
+  def handle_event("pagination", %{"page" => _} = params, socket) do
+    path = ITJWeb.Router.Helpers.live_path(socket, ITJWeb.OffersLive, params)
+    socket = push_patch(socket, to: path)
+    {:noreply, socket}
+  end
 end
--- lib/itj_web/views/page.ex
+++ lib/itj_web/views/page.ex
@@ -1,3 +1,11 @@
 defmodule ITJWeb.PageView do
   use ITJWeb, :view
+
+  def get_page(%{"page" => page}) do
+    String.to_integer(page)
+  end
+
+  def get_page(_) do
+    1
+  end
 end
